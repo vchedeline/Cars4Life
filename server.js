5dbae8f5d8b4f7dd245b99309111100a58150b14
chedeline <vchedeline@gmail.com>
2022-04-27 18:18:05 -0400

    without morgan

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const express = require("express");
 // const session = require("express-session");
 const methodOverride = require("method-override");
 // const MongoStore = require("connect-mongo");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 8008;
 const carRoutes = require("./controllers/CarCtrls");
 const carMain = require("./controllers/main");
 
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
